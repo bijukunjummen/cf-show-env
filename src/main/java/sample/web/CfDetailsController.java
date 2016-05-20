afb94ff21989903443cbdda133bd691e9eba92cb
Biju Kunjummen <bkunjummen@Bijus-MacBook-Pro.local>
2016-05-19 17:26:06 -0700

    Sample CI Flow

---
+++ src/main/java/sample/web/CfDetailsController.java
@@ -0,0 +1,23 @@
+package sample.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CfDetailsController {
+    @Autowired
+    private Environment environment;
+
+    @RequestMapping("/")
+    public String vcapProperties(Model model) {
+        model.addAttribute("cfapp", environment.getProperty("VCAP_APPLICATION", "{}"));
+        model.addAttribute("cfservices", environment.getProperty("VCAP_SERVICES", "{}"));
+        model.addAttribute("cfenvs", System.getenv());
+        model.addAttribute("activeProfiles", environment.getActiveProfiles());
+        return "index";
+    }
+
+}
