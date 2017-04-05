f5bfbebad88416a12fcdd769d2a694b6765362c2
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2017-04-05 09:42:59 -0500

    With a load test scenario with gatling

---
+++ src/simulations/scala/simulations/SimpleSimulation.scala
@@ -0,0 +1,27 @@
+package simulations
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class SimpleSimulation extends Simulation {
+
+  val baseUrl = s"http://cf-show-env.cfapps.io";
+
+  val httpConf = http.baseURL(baseUrl)
+
+  val headers = Map("Accept" -> """application/json""")
+
+  val healthPage = repeat(25) {
+    exec(http("health")
+      .get("/health"))
+      .pause(2, 2)
+  }
+
+  val scn = scenario("Get the Health Page")
+    .exec(healthPage)
+
+
+
+  setUp(scn.inject(atOnceUsers(5)).protocols(httpConf))
+}
